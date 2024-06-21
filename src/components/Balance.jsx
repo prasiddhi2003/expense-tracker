c54a6b1d8edb9d3d1272ac581523a9c0f9cd9956
PRASIDDHI <pdm@example.com>
2024-06-21 17:18:00 +0530

    first commit

---
+++ src/components/Balance.jsx
@@ -0,0 +1,20 @@
+import {Box,Typography,styled} from '@mui/material'
+
+const BalanceText=styled(Typography)`
+font-size:25px;
+margin-bottom:20px;
+`
+
+const Balance=({transactions}) => {
+
+    const amount=transactions.map(transaction=>transaction.amount);
+    const total=amount.reduce((amount,item) => (amount+=item) , 0).toFixed(2);
+    
+    return(
+        <Box>
+        <Typography>Balance:₹{total}</Typography>
+        </Box>
+    )
+}
+
+export default Balance;
